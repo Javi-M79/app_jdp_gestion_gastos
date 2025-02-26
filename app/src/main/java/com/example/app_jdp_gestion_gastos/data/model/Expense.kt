9c87ce35acc363bd2cf9f31c2cf6b5b6220d7200
Silx79 <javier.mira46@gmail.com>
2025-02-26 07:45:29 +0100

    commit

--- app/src/main/java/com/example/app_jdp_gestion_gastos/data/model/Expense.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/data/model/Expense.kt
@@ -1,20 +1,19 @@
 package com.example.app_jdp_gestion_gastos.data.model
 
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.PropertyName
 
 
-class expense(
+class Expense(
 
     @get:PropertyName("userId") @set:PropertyName("userId") var userId: String = "",
     @get:PropertyName("name") @set:PropertyName("name") var name: String = "",
     @get:PropertyName("category") @set:PropertyName("category") var category: String = "",
-    @get:PropertyName("amount") @set:PropertyName("amount") var amount: String = "",
-    @get:PropertyName("date") @set:PropertyName("date") var date: String = "",
+    @get:PropertyName("amount") @set:PropertyName("amount") var amount: Double = 0.0,
+    @get:PropertyName("date") @set:PropertyName("date") var date: Timestamp? = null,
     @get:PropertyName("importance") @set:PropertyName("importance") var importance: String = "",
     @get:PropertyName("colorCode") @set:PropertyName("colorCode") var colorCode: String = "",
     @get:PropertyName("isRecurring") @set:PropertyName("isRecurring") var isRecurring: Boolean = false,
-    @get:PropertyName("recurrence") @set:PropertyName("userId") var recurrence: String = "",
+    @get:PropertyName("recurrence") @set:PropertyName("recurrence") var recurrence: String = "",
 
-
-    ) {
-}
+    )
