3c4490a4212cdcb7ae12da7087da97bbbbf05fe1
Silx79 <javier.mira46@gmail.com>
2025-02-26 07:46:42 +0100

    creado modelo para grupos

--- app/src/main/java/com/example/app_jdp_gestion_gastos/data/model/Group.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/data/model/Group.kt
@@ -1,5 +1,14 @@
 package com.example.app_jdp_gestion_gastos.data.model
 
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.PropertyName
 
-class Group {
-}
+
+data class Group(
+    @get:PropertyName("name") @set:PropertyName("name") var name: String = "",
+    @get:PropertyName("createdBy") @set:PropertyName("createdBy") var createdBy: String = "",
+    @get:PropertyName("members") @set:PropertyName("members") var members: List<String> = emptyList(),
+    @get:PropertyName("createdAt") @set:PropertyName("createdAt") var createdAt: Timestamp? = null,
+    @get:PropertyName("expenses") @set:PropertyName("expenses") var expenses: List<String> = emptyList(),
+
+    )
