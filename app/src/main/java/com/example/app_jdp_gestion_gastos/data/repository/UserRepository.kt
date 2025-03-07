918cf10c7b3f6b016f683f9890bcad788c7b8a39
Silx79 <javier.mira46@gmail.com>
2025-03-07 12:41:22 +0100

    fix code

--- app/src/main/java/com/example/app_jdp_gestion_gastos/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/data/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.example.app_jdp_gestion_gastos.data.repository
 
+import android.util.Log
 import com.example.app_jdp_gestion_gastos.data.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -13,30 +14,28 @@ import kotlinx.coroutines.tasks.await
     */
 
 
-class UserRepository {
+class UserRepository( private val auth: FirebaseAuth = FirebaseAuth.getInstance(),
+        private val db: FirebaseFirestore = FirebaseFirestore.getInstance()) {
 
-    //Inicio de firebase
-
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
 
 
     //REGISTRO USUARIO
     //suspend: Funcion suspendida para la ejecucion de operaciones asincronas o en segundo plano.
 
-    suspend fun registerUser(email: String, password: String, name: String? = null): String? {
+    suspend fun registerUser(email: String, password: String, name: String): String? {
         return try {
             //Desde el reposotorio conectamos con el auth de firebase.
-            //Crear usuario en Firebase Authentication.
 
+            //Crear usuario en Firebase Authentication.
+            Log.e("UserRepository", "🔹 Intentando registrar usuario en Firebase Authentication con email: $email")
             val authResult = auth.createUserWithEmailAndPassword(email, password).await()
             val userId = authResult.user?.uid ?: return null
+            Log.e("UserRepository", "✅ Usuario registrado con UID en Firebase: $userId")
 
             // Crear usuario en Firestore usando su UID como ID del documento
             val newUser = User(
-                name = name
-                    ?: "", //No pedimos este dato en el registro, lo dejamos en blanco. TODO Tomar decision de implementarlo
+                name = name,
                 mail = email,
                 profilePicture = "",
                 balance = 0.0,
@@ -63,8 +62,6 @@ class UserRepository {
         }
 
     }
-
-
 
 
     //Obtener datos de usuario autenticado
