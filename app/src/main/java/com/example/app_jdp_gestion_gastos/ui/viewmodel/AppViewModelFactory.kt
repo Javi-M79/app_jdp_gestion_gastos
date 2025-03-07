52eb9be95f8d0ae354ac309e7ed98ff69aa819b4
Silx79 <javier.mira46@gmail.com>
2025-03-07 12:40:06 +0100

    Creado AppViewModelFactory para el instanciar los repositories

---
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/ui/viewmodel/AppViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.example.app_jdp_gestion_gastos.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.app_jdp_gestion_gastos.data.repository.UserRepository
+
+
+//Esta clase se encargara de proporcionar instancias de los repositorios al ViewModel.
+//Ha dado problemas al instanciarlo desde el ViewModel.
+
+class AppViewModelFactory(private val userRepository: UserRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(UserViewModel::class.java) -> UserViewModel(userRepository) as T
+            else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+        }
+    }
+
+
+}
