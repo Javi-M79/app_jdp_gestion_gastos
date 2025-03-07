2efd3a93add77ec188dac19bc873b9021a212b19
Silx79 <javier.mira46@gmail.com>
2025-03-07 12:39:25 +0100

    Implementado registro de usuario correctamente

--- app/src/main/java/com/example/app_jdp_gestion_gastos/ui/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/ui/activities/RegistroActivity.kt
@@ -2,22 +2,35 @@ package com.example.app_jdp_gestion_gastos.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.app_jdp_gestion_gastos.data.repository.UserRepository
 import com.example.app_jdp_gestion_gastos.databinding.ActivityRegistroBinding
+import com.example.app_jdp_gestion_gastos.ui.viewmodel.AppViewModelFactory
 import com.example.app_jdp_gestion_gastos.ui.viewmodel.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegistroActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
 
     //David he implementado binding en esta activity
     private lateinit var binding: ActivityRegistroBinding
 
+    private val userRepository by lazy {
+        UserRepository(
+            FirebaseAuth.getInstance(),
+            FirebaseFirestore.getInstance()
+        )
+    }
+
     // Variable para acceder al ViewModel.
-    private val userViewModel: UserViewModel by viewModels()
+    private val userViewModel: UserViewModel by viewModels {
+        AppViewModelFactory(userRepository)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,51 +38,42 @@ class RegistroActivity : AppCompatActivity() {
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.e("Iniciada RegistroActivity", "🔹 RegistroActivity iniciada")
+
         //Incializacion de variables que interactuaran con el layout.
-        val btnRegistrar = binding.btnRegistrar
+        val btnRegistrar = binding.tvRegistro
         val tvVolverLogin = binding.tvVolverLogin
 
-        // Vinculacion de variables con elementos del layout.
-        /* val etMail = binding.etMailRegistro.text.toString().trim()
-         val etPassword = binding.etPasswordRegistro.text.toString().trim()
-         val etPasswordConfirm = binding.etPasswordConfirm.text.toString().trim()
-         */
+        /*    btnRegistrar.setOnClickListener {
+                Snackbar.make(it, "Registro exitoso", Snackbar.LENGTH_SHORT).show()
+            }*/
 
 
-        // Manejo de registro de usuarios.
+        //REGISTRO DE USUARIOS
+
         btnRegistrar.setOnClickListener {
             //Variables que se pasaran por parametro a la funcion de registro.
+            val name = binding.etNombreRegistro.text.toString().trim()
             val email = binding.etMailRegistro.text.toString().trim()
             val password = binding.etPasswordRegistro.text.toString().trim()
             val confirmPassword = binding.etPasswordConfirm.text.toString().trim()
 
-            //Validaciones de campos vacios.
-            if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT)
-                    .show()
-                return@setOnClickListener
-            }
-
-            //Validaciones de contraseñas.
-            else if (password != confirmPassword) {
-                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            //Registro de usuario.
-            else {
-                userViewModel.registerUser(email, password, null) { userId, error ->
-                    if (userId != null) {
-                        Toast.makeText(this, "Usuario registrado con exito", Toast.LENGTH_SHORT)
-                            .show()
-                        startActivity(Intent(this, HomeActivity::class.java))
-                        finish()
-                    } else {
-                        Toast.makeText(this, "Error en el registro", Toast.LENGTH_SHORT).show()
-                    }
+            Log.e("RegistroActivity", "🔹 Botón de registrar presionado con email: $email")
+            userViewModel.registerUser(email, password, confirmPassword, name) { userId, error ->
+                if (userId != null) {
+                    Log.e("RegistroActivity", "✅ Registro exitoso con UID: $userId")
+                    Toast.makeText(this, "Usuario registrado con exito", Toast.LENGTH_SHORT)
+                        .show()
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    finish()
+                } else {
+                    Log.e("RegistroActivity", "❌ Error en RegistroActivity: $error")
+                    Toast.makeText(this, error ?: "Error desconocido", Toast.LENGTH_SHORT).show()
                 }
             }
+
         }
+
 
         // Boton para volver a la pantalla de login.
         tvVolverLogin.setOnClickListener {
