7a99681d269cb3987e08414c8be0c0e408fe896d
Silx79 <javier.mira46@gmail.com>
2025-03-09 10:53:57 +0100

    creados fragment para mejor manejo de la app

--- app/src/main/java/com/example/app_jdp_gestion_gastos/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/ui/fragments/LoginFragment.kt
@@ -1,60 +1,124 @@
 package com.example.app_jdp_gestion_gastos.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.app_jdp_gestion_gastos.R
+import com.example.app_jdp_gestion_gastos.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        // TODO: VARIABLES
+        // Animación del titulo
+        val titulo = binding.tvTitulo
+        val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+        titulo.startAnimation(fadeIn)
+
+
+        val login = binding.btnLogin
+        val resetPassword = binding.resetpassword
+        val fondo = binding.ivFondo
+        val crearCuenta = binding.tvCrearCuenta
+
+
+        crearCuenta.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registroFragment)
+        }
+
+        login.setOnClickListener {
+            //Variables para el login.
+            val mail = binding.etMail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            //Login en Firebase
+
+
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
     }
+
+    /*EVENTOS FOCUS MAIL Y PASWORD
+    etMail.setOnFocusChangeListener { _, hasFocus ->
+        if (hasFocus) {
+            // Zoom In
+            val zoomIn = ScaleAnimation(
+                1.0f, 1.2f, // Escala en X
+                1.0f, 1.2f, // Escala en Y
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+            ).apply {
+                duration = 300 // Duración en milisegundos
+                fillAfter = true // mantien el estado final
+            }
+            fondoImagen.startAnimation(zoomIn)
+        } else {
+            // Zoom Out
+            val zoomOut = ScaleAnimation(
+                1.2f, 1.0f,
+                1.2f, 1.0f,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+            ).apply {
+                duration = 300
+                fillAfter = true
+            }
+            fondoImagen.startAnimation(zoomOut)
+        }
+    }
+    etPassword.setOnFocusChangeListener { _, hasFocus ->
+        if (hasFocus) {
+            // Zoom In
+            val zoomIn = ScaleAnimation(
+                1.0f, 1.2f, // Escala en X
+                1.0f, 1.2f, // Escala en Y
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+            ).apply {
+                duration = 300 // Duración en milisegundos
+                fillAfter = true // mantien el estado final
+            }
+            fondoImagen.startAnimation(zoomIn)
+        } else {
+            // Zoom Out
+            val zoomOut = ScaleAnimation(
+                1.2f, 1.0f,
+                1.2f, 1.0f,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+            ).apply {
+                duration = 300
+                fillAfter = true
+            }
+            fondoImagen.startAnimation(zoomOut)
+        }
+    }*/
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
