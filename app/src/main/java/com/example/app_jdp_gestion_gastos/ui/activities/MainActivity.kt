4d8ba05a17dd388fca006b40738496ffc888a48d
Silx79 <javier.mira46@gmail.com>
2025-03-09 10:54:24 +0100

    adaptado a fragments nuevos

--- app/src/main/java/com/example/app_jdp_gestion_gastos/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/ui/activities/MainActivity.kt
@@ -1,53 +1,27 @@
 package com.example.app_jdp_gestion_gastos.ui.activities
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.animation.AnimationUtils
-import android.view.animation.ScaleAnimation
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.app_jdp_gestion_gastos.R
 import com.example.app_jdp_gestion_gastos.databinding.ActivityMainBinding
-import com.example.app_jdp_gestion_gastos.ui.viewmodel.UserViewModel
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    // Variable que almacena el ViewModel para trasladar los datos a la vista.
-    private val userViewModel: UserViewModel by viewModels()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        //INICIALIZACION DE VARIABLES
-        val login: Button = binding.btnLogin
-        val register: TextView = binding.tvCrearCuenta
-
-
-        register.setOnClickListener {
-            val intent = Intent(this, RegistroActivity::class.java)
-            startActivity(intent)
-
-        }
-
-
-        login.setOnClickListener {
-
-            val mail: String = binding.etMail.text.toString().trim()
-            val password: String = binding.etPassword.text.toString().trim()
-
-
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
 
 
         // TODO: Activar modo inmersivo (Desactivar barra de estado)
@@ -59,17 +33,17 @@ class MainActivity : AppCompatActivity() {
                 )
 
 
-        // TODO: VARIABLES
-        // Animación del titulo
-        val titulo = findViewById<TextView>(R.id.tvTitulo)
-        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-        titulo.startAnimation(fadeIn)
-        val etMail = findViewById<EditText>(R.id.etMail)
-        val etPassword = findViewById<EditText>(R.id.etPassword)
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val tvOlvidarPassword = findViewById<TextView>(R.id.tvOlvidarContraseña)
-        val fondoImagen = findViewById<ImageView>(R.id.ivFondo)
-        val tvCrearCuenta = findViewById<TextView>(R.id.tvCrearCuenta)
+        /* // TODO: VARIABLES
+         // Animación del titulo
+         val titulo = findViewById<TextView>(R.id.tvTitulo)
+         val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+         titulo.startAnimation(fadeIn)
+         val etMail = findViewById<EditText>(R.id.etMail)
+         val etPassword = findViewById<EditText>(R.id.etPassword)
+         val btnLogin = findViewById<Button>(R.id.btnLogin)
+         val tvOlvidarPassword = findViewById<TextView>(R.id.tvOlvidarContraseña)
+         val fondoImagen = findViewById<ImageView>(R.id.ivFondo)
+         val tvCrearCuenta = findViewById<TextView>(R.id.tvCrearCuenta)*/
 
 
         // Eventos de boton login
@@ -100,68 +74,6 @@ class MainActivity : AppCompatActivity() {
              }
          }*/
 
-
-        //TODO REVISAR CAMBIAR A FRAGMENT REGISTROACTIVITY
-        tvCrearCuenta.setOnClickListener {
-            val intent = Intent(this, RegistroActivity::class.java)
-            startActivity(intent)
-        }
-
-        // EVENTOS FOCUS MAIL Y PASWORD
-        etMail.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                // Zoom In
-                val zoomIn = ScaleAnimation(
-                    1.0f, 1.2f, // Escala en X
-                    1.0f, 1.2f, // Escala en Y
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-                ).apply {
-                    duration = 300 // Duración en milisegundos
-                    fillAfter = true // mantien el estado final
-                }
-                fondoImagen.startAnimation(zoomIn)
-            } else {
-                // Zoom Out
-                val zoomOut = ScaleAnimation(
-                    1.2f, 1.0f,
-                    1.2f, 1.0f,
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-                ).apply {
-                    duration = 300
-                    fillAfter = true
-                }
-                fondoImagen.startAnimation(zoomOut)
-            }
-        }
-        etPassword.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                // Zoom In
-                val zoomIn = ScaleAnimation(
-                    1.0f, 1.2f, // Escala en X
-                    1.0f, 1.2f, // Escala en Y
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-                ).apply {
-                    duration = 300 // Duración en milisegundos
-                    fillAfter = true // mantien el estado final
-                }
-                fondoImagen.startAnimation(zoomIn)
-            } else {
-                // Zoom Out
-                val zoomOut = ScaleAnimation(
-                    1.2f, 1.0f,
-                    1.2f, 1.0f,
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                    ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-                ).apply {
-                    duration = 300
-                    fillAfter = true
-                }
-                fondoImagen.startAnimation(zoomOut)
-            }
-        }
 
     }
 
@@ -206,4 +118,8 @@ class MainActivity : AppCompatActivity() {
                  }
              }
      }*/
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
