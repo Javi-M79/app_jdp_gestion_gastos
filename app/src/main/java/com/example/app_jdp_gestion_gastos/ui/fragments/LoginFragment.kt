7be8cd98244e56e2edb0c9f3adc378c4a737f0f1
Silx79 <javier.mira46@gmail.com>
2025-03-10 16:13:21 +0100

    implementado login firebase

--- app/src/main/java/com/example/app_jdp_gestion_gastos/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/ui/fragments/LoginFragment.kt
@@ -5,16 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.app_jdp_gestion_gastos.R
+import com.example.app_jdp_gestion_gastos.data.repository.UserRepository
 import com.example.app_jdp_gestion_gastos.databinding.FragmentLoginBinding
+import com.example.app_jdp_gestion_gastos.ui.viewmodel.AppViewModelFactory
+import com.example.app_jdp_gestion_gastos.ui.viewmodel.UserViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    //Inicio del UserRepository
+    private val userRepository by lazy {
+        UserRepository(
+            FirebaseAuth.getInstance(),
+            FirebaseFirestore.getInstance()
+        )
+
+    }
+
+    //Inicio del UserViewModel que recibe el reposotory a traves de ViewmOdel factori
+    private val userViewModel: UserViewModel by viewModels {
+        AppViewModelFactory(userRepository)
+    }
 
 
     override fun onCreateView(
@@ -27,7 +48,6 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
 
         // TODO: VARIABLES
         // Animación del titulo
@@ -42,83 +62,92 @@ class LoginFragment : Fragment() {
         val crearCuenta = binding.tvCrearCuenta
 
 
+        //LOGIN EN FIREBASE
+        login.setOnClickListener {
+            //Variables  necesarias para el login.
+            val mail = binding.etMail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            //Metodo en el userViewModel
+            userViewModel.loginUser(mail, password) { userId, error ->
+                if (userId != null) {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    Toast.makeText(requireContext(), "Usuario no encontrado", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+        }
+
         crearCuenta.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registroFragment)
         }
 
-        login.setOnClickListener {
-            //Variables para el login.
-            val mail = binding.etMail.text.toString().trim()
-            val password = binding.etPassword.text.toString().trim()
-
-            //Login en Firebase
-
-
-            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-        }
     }
-
-    /*EVENTOS FOCUS MAIL Y PASWORD
-    etMail.setOnFocusChangeListener { _, hasFocus ->
-        if (hasFocus) {
-            // Zoom In
-            val zoomIn = ScaleAnimation(
-                1.0f, 1.2f, // Escala en X
-                1.0f, 1.2f, // Escala en Y
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-            ).apply {
-                duration = 300 // Duración en milisegundos
-                fillAfter = true // mantien el estado final
-            }
-            fondoImagen.startAnimation(zoomIn)
-        } else {
-            // Zoom Out
-            val zoomOut = ScaleAnimation(
-                1.2f, 1.0f,
-                1.2f, 1.0f,
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-            ).apply {
-                duration = 300
-                fillAfter = true
-            }
-            fondoImagen.startAnimation(zoomOut)
-        }
-    }
-    etPassword.setOnFocusChangeListener { _, hasFocus ->
-        if (hasFocus) {
-            // Zoom In
-            val zoomIn = ScaleAnimation(
-                1.0f, 1.2f, // Escala en X
-                1.0f, 1.2f, // Escala en Y
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-            ).apply {
-                duration = 300 // Duración en milisegundos
-                fillAfter = true // mantien el estado final
-            }
-            fondoImagen.startAnimation(zoomIn)
-        } else {
-            // Zoom Out
-            val zoomOut = ScaleAnimation(
-                1.2f, 1.0f,
-                1.2f, 1.0f,
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-                ScaleAnimation.RELATIVE_TO_SELF, 0.5f
-            ).apply {
-                duration = 300
-                fillAfter = true
-            }
-            fondoImagen.startAnimation(zoomOut)
-        }
-    }*/
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
+
+/*EVENTOS FOCUS MAIL Y PASWORD
+etMail.setOnFocusChangeListener { _, hasFocus ->
+    if (hasFocus) {
+        // Zoom In
+        val zoomIn = ScaleAnimation(
+            1.0f, 1.2f, // Escala en X
+            1.0f, 1.2f, // Escala en Y
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+        ).apply {
+            duration = 300 // Duración en milisegundos
+            fillAfter = true // mantien el estado final
+        }
+        fondoImagen.startAnimation(zoomIn)
+    } else {
+        // Zoom Out
+        val zoomOut = ScaleAnimation(
+            1.2f, 1.0f,
+            1.2f, 1.0f,
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+        ).apply {
+            duration = 300
+            fillAfter = true
+        }
+        fondoImagen.startAnimation(zoomOut)
+    }
+}
+etPassword.setOnFocusChangeListener { _, hasFocus ->
+    if (hasFocus) {
+        // Zoom In
+        val zoomIn = ScaleAnimation(
+            1.0f, 1.2f, // Escala en X
+            1.0f, 1.2f, // Escala en Y
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+        ).apply {
+            duration = 300 // Duración en milisegundos
+            fillAfter = true // mantien el estado final
+        }
+        fondoImagen.startAnimation(zoomIn)
+    } else {
+        // Zoom Out
+        val zoomOut = ScaleAnimation(
+            1.2f, 1.0f,
+            1.2f, 1.0f,
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+            ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+        ).apply {
+            duration = 300
+            fillAfter = true
+        }
+        fondoImagen.startAnimation(zoomOut)
+    }
+}*/
+
+
+
+
+
