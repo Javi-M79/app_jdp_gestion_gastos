75e495b320e15ec30dfb5b964961469d9401a27a
Silx79 <javier.mira46@gmail.com>
2025-03-15 06:36:44 +0100

    Borrados Controller

--- app/src/main/java/com/example/app_jdp_gestion_gastos/controller/ExpensesController.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app_jdp_gestion_gastos.controller
-
-//Logica para la gestion de gastos
-
-class ExpensesController {
-}
--- app/src/main/java/com/example/app_jdp_gestion_gastos/controller/GroupController.kt
+++
@@ -1,7 +0,0 @@
-package com.example.app_jdp_gestion_gastos.controller
-
-
-//Logica para la gestion de grupos
-
-class GroupController {
-}
