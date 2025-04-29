c9d286c05e15dc18122730f2cb6b46836f819307
Daserra23 <daserra23@gmail.com>
2025-04-29 16:19:21 +0200

    CORRECCIÓN
    - Corregido fragmentGroups. Ahora se puede añadir y borrar personas al grupo.
    
    - Corregido Cuadro de dialogo tambien.

--- app/src/main/java/com/example/app_jdp_gestion_gastos/data/repository/GroupRepository.kt
+++ app/src/main/java/com/example/app_jdp_gestion_gastos/data/repository/GroupRepository.kt
@@ -1,6 +1,9 @@
 package com.example.app_jdp_gestion_gastos.data.repository
 
+import android.util.Log
 import com.example.app_jdp_gestion_gastos.data.model.Group
+import com.example.app_jdp_gestion_gastos.data.model.User
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 import kotlinx.coroutines.tasks.await
@@ -8,6 +11,7 @@ import kotlinx.coroutines.tasks.await
 class GroupRepository {
     private val db = FirebaseFirestore.getInstance()
     private val groupCollection = db.collection("groups")
+    private val userCollection = db.collection("users") // AÑADIDO
 
     // Crear un nuevo grupo en Firestore
     suspend fun createGroup(group: Group): String? {
@@ -35,5 +39,38 @@ class GroupRepository {
                 groupRef.update("members", updatedMembers).await()
             }
         }
+    }
+
+    /// Obtener usuarios por lista de IDs
+    suspend fun getUsersByIds(userIds: List<String>): List<User> {
+        val users = mutableListOf<User>()
+        try {
+            val snapshots = userCollection.whereIn(FieldPath.documentId(), userIds).get().await()
+            snapshots.documents.forEach { snapshot ->
+                snapshot.toObject(User::class.java)?.let {
+                    it.id = snapshot.id // 👈 Asignar UID manualmente
+                    users.add(it)
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("GroupRepository", "Error al obtener usuarios: ${e.message}", e)
+        }
+        Log.d("GroupRepository", "Usuarios obtenidos: ${users.size}")
+        return users
+    }
+
+    // Obtener todos los usuarios
+    suspend fun getAllUsers(): List<User> {
+        val snapshot = userCollection.get().await()
+        return snapshot.documents.mapNotNull { doc ->
+            doc.toObject(User::class.java)?.apply {
+                id = doc.id // 👈 Asignar UID manualmente
+            }
+        }
+    }
+
+    // Actualizar los miembros del grupo
+    suspend fun updateGroupMembers(groupId: String, members: List<String>) {
+        groupCollection.document(groupId).update("members", members).await()
     }
 }
